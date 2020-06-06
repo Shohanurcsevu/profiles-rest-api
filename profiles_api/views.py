86418144d0def00e22dd3ff4b36e5181517d2b93
Shohanur Rahman <shohanurvu@gmail.com>
2020-06-05 20:04:54 -0700

    Hello Api View in Django

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response 
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+
+    def get(self,request, format=None):
+        """Returns a list if ApiBView Features"""
+        an_apiview = [
+            'Uses HTTP mehtods as function (get post,patch,put,delete'
+            ''
+        ]
+
+        return Response({'message': 'hello', 'an_apiview':an_apiview})
